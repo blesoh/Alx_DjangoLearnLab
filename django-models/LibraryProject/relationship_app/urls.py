3dd2394bfa320bdc8f91efea16aed2efdb4d7a0f
blesoh <rapandoblessing@gmail.com>
2025-11-09 15:06:27 -0600

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,9 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import register
-
-from django.shortcuts import redirect
-
-# Simple root redirect to login
-def root_redirect(request):
-    return redirect('login')
+from . import views
 
 urlpatterns = [
-    path('', root_redirect, name='root'),  # Redirect root URL to login
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
