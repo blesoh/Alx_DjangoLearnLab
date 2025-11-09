713622896bc61ffe9b070ea713004db076d23643
blesoh <rapandoblessing@gmail.com>
2025-11-09 13:28:43 -0600

    add template for list_books.html

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Book
 
 def list_books(request):
-    """
-    Return a simple text list of book titles and their authors.
-    """
-    books = Book.objects.select_related('author').all()
-    lines = ["List of Books:"]
-    for b in books:
-        lines.append(f"{b.title} by {b.author.name}")
-    text = "\n".join(lines)
-    return HttpResponse(text, content_type="text/plain")
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
