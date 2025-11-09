ae6f6783db79aa7b98e666c7e7e37841cf6f22d6
blesoh <rapandoblessing@gmail.com>
2025-11-09 13:52:39 -0600

    Add Library import for class-based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,20 @@
+# LibraryProject/relationship_app/views.py
 
 from django.shortcuts import render
 from django.views import View
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
-# Function-based view
+# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
+    output = ""
+    for book in books:
+        output += f"{book.title} by {book.author.name}\n"
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view
+# Class-based view to show details of a specific library
 class LibraryDetailView(View):
     def get(self, request, pk):
-        library = Library.objects.get(id=pk)
+        library = Library.objects.get(pk=pk)
         return render(request, 'relationship_app/library_detail.html', {'library': library})
-
-
