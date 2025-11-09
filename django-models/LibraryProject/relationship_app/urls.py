f0c5f98e595bbc65a5b40679fd604635594ef972
blesoh <rapandoblessing@gmail.com>
2025-11-09 14:02:14 -0600

    Add URL patterns for list_books and LibraryDetailView

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('', views.list_books, name='list_books'),
-    path('<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('', list_books, name='list_books'),  # Function-based view
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
 ]
+
 
